02ee90ca00b425714d320c2c345fd3bd5b15f8a5
Bruce Chen <662005@qq.com>
2013-03-14 11:10:11 +0800

    修改config.php

--- Examples/config.php
+++ Examples/config.php
@@ -2,6 +2,6 @@
 
 include_once('../Vdisk/Vdisk.php');
 
-define('VDISK_CLIENT_ID', '1586711366');
-define('VDISK_CLIENT_SECRET', 'c7c3711444291fffed9d4dbf469ee60f');
-define('VDISK_CALLBACK_URL', 'http://vauth.appsina.com/Examples/callback.php');
+define('VDISK_CLIENT_ID', 'xxxxxxxxxx');
+define('VDISK_CLIENT_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+define('VDISK_CALLBACK_URL', 'http://xxxxxxxxxxxxxxxxxxxxxxxxxx');
